23a2e86d5c4505ec4a02beb4ab15556b8c400f7b
Radu <aliment4476@gmail.com>
2025-11-01 18:31:05 +0100

    Added main menu

---
+++ Assets/Scripts/Misc/Menus.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
